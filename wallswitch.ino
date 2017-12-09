d27da75198c96aef3889564cb2b9b8d65c9704d3
Satoshi N. M <snmatsutake@yahoo.co.jp>
2017-12-08 19:45:51 -0800

    Delay ADC read to fix https://arduino.stackexchange.com/questions/19787/esp8266-analog-read-interferes-with-wifi

---
+++ wallswitch.ino
@@ -0,0 +1,110 @@
+// wallswitch
+
+#include <ESP8266WiFi.h>
+#include <WiFiUdp.h>
+#include <WiFiClient.h>
+#include <ESP8266WebServer.h>
+#include <ESP8266mDNS.h>
+
+/* edit to your network:
+const char* ssid = "..";
+const char* password = "...";
+const char* udp_recipient = "192.168.0.111";
+const int udp_port = 8266;
+*/
+#include "creds.h"
+
+ESP8266WebServer server(80);
+
+#define SWITCH_PIN D4
+#define INPUT_ON_PACKET  "\xe1"
+#define INPUT_OFF_PACKET "\xe0"
+
+static int last_input = 0;
+static int input = 0;
+static int analog_value = 0;
+
+void handleRoot() {
+  String html = "<!DOCTYPE html>\n"
+"<html>\n"
+"<head>\n"
+"<title>Wallswitch</title>\n"
+"</head>\n"
+"<meta charset=\"UTF-8\">\n"
+"<body>\n"
+"<h1>wallswitch</h1>\n"
+"<p>Analog input: " + String(analog_value) + "\n" +
+"<p>Switch input: " + String(input) + "\n" +
+"</body>\n"
+"</html>\n";
+
+  server.send(200, "text/html", html);
+}
+
+void handleNotFound() {
+  String message = "Not found: " + server.uri();
+
+  server.send(404, "text/plain", message);
+}
+
+void setup() {
+  pinMode(BUILTIN_LED, OUTPUT);
+  digitalWrite(BUILTIN_LED, 0);
+  Serial.begin(115200);
+  WiFi.begin(ssid, password);
+  Serial.println("\nWaiting to connect to Wi-Fi...");
+
+  // Wait for connection
+  int toggle = 1;
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(100);
+    Serial.print(".");
+    digitalWrite(BUILTIN_LED, toggle);
+    toggle = !toggle;
+  }
+  Serial.println("");
+  Serial.print("Connected to ");
+  Serial.println(ssid);
+  Serial.print("IP address: ");
+  Serial.println(WiFi.localIP());
+  digitalWrite(BUILTIN_LED, 1);
+
+  if (MDNS.begin("wallswitch")) {
+    Serial.println("MDNS responder started");
+  }
+
+  server.on("/", handleRoot);
+
+  server.onNotFound(handleNotFound);
+
+  server.begin();
+  Serial.println("HTTP server started");
+
+  pinMode(SWITCH_PIN, INPUT);
+}
+
+static WiFiUDP udp;
+
+void loop() {
+  server.handleClient();
+
+  if (millis() % 50 == 0) {
+    analog_value = analogRead(A0);
+  }
+
+  input = digitalRead(SWITCH_PIN);
+
+  if (input != last_input) {
+    Serial.println("Input changed: " + String(last_input) + " -> " + String(input));
+
+    udp.beginPacket(udp_recipient, udp_port);
+    if (input) {
+      udp.write(INPUT_ON_PACKET, sizeof(INPUT_ON_PACKET) - 1);
+    } else {
+      udp.write(INPUT_OFF_PACKET, sizeof(INPUT_OFF_PACKET) - 1);
+    }
+    udp.endPacket();
+  }
+
+  last_input = input;
+}
